0e296ed6cd5f931db4f37a814631d5b0b0d5a38b
Chief-Baliman <80815977+Chief-Baliman@users.noreply.github.com>
2026-06-14 18:23:24 +0200

    Update data.js

--- data.js
+++ data.js
@@ -37,7 +37,7 @@ window.QUIZT_DATA = {
       time: "19:30 Uhr",
       city: "Dormagen",
       ticketUrl: "https://rausgegangen.de/events/quizt-streetlife-das-quiz-mit-twist-0/",
-      scoreboardCode: "",
+      scoreboardCode: "7507",
       flyerImage: "flyer-streetlife.png",
       status: "upcoming",
       featured: true
